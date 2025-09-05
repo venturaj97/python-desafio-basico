d0ebefd5a6613794f98c8b14349ce6c888e8dd81
João Victor Ventura Mendes <joaoventura97@outlook.com>
2025-09-05 02:43:46 -0300

    desafio_2

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('teste2-github.2')
+#Mini desafio: Calcular quantos dias duraria um produto se a pessoa usar X porções por dia
+
+
+total_porcao = int(input('Digite quantas porções no total tem o produto? '))
+dia_porcao = int(input('Digite quantas porções são consumidas no dia? '))
+
+calculo_duracao = total_porcao / dia_porcao
+
+print(f'O produto vai durar {calculo_duracao:.0f} dias!')
